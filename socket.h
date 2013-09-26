1b7428f2c96ba8b56189e5b69acc6d2b810bd210
fads93 <fads93@gmail.com>
2013-09-26 10:59:31 +0000

    Sender address resolution.

--- socket.h
+++ socket.h
@@ -1,11 +1,31 @@
 #pragma once
+#include <sys/types.h>
+#include <sys/socket.h>
 
-class socket_listener
+class net_connection
 {
     private:
         int sock;
+        struct sockaddr_storage plane_addr;
+
     public:
-        char* get_buffer( );
-        socket_listener( );
-        ~socket_listener( );
+        /**
+         * Bind address for server.
+         */
+        net_connection( );
+
+        /**
+         * Close socket.
+         */
+        ~net_connection( );
+
+        /**
+         * Listen on socket and memorize sender address. 
+         */
+        char* receive( );
+
+        /**
+         * Send prepared buffer to client.
+         */
+        void send( char* buf);
 };
