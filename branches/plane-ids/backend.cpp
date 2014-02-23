febfffd137f709fc52f1a68cb70eaff86887b74e
fads93@gmail.com <fads93@gmail.com@6ab701a5-40d5-667e-f451-28f2601707fb>
2014-02-23 13:32:06 +0000

    Run database poller in separate thread

--- branches/plane-ids/backend.cpp
+++ branches/plane-ids/backend.cpp
@@ -1,26 +1,32 @@
 #include "logger.h"
 #include "network.h"
 #include "handler.h"
-#include <unistd.h>
+#include <thread>
+
+void db_poll(database* db)
+{
+    while (true)
+    {
+        db->command_poll();
+        sleep(1);
+    }
+}
 
 int main() {
+    /* Database connection */
+    database* db = new database;
     try
     {
-        /* Database connection */
-        database* db = new database;
         /* Create network message handler object */
         handler network_dispatcher(db);
         /* Create a server object to accept incoming client requests, 
          * and run the io_service object. */
         boost::asio::io_service io_service;
         udp_server server(io_service, network_dispatcher);
-        io_service.run();
         /* Run DB poller, in a loop :( */
-        while (true)
-        {
-            db->command_poll();
-            sleep(1);
-        }
+        std::thread db_poll_thread(db_poll, db);
+        db_poll_thread.join();
+        io_service.run();
     }
     catch (std::exception& e)
     {
